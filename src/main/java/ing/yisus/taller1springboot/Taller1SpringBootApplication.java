96618ecfc82874f09c91db559d573c2a1850f6f4
Jesus Antonio Murcia <yisusmurci@gmail.com>
2025-05-19 18:46:12 -0500

    Project created

---
+++ src/main/java/ing/yisus/taller1springboot/Taller1SpringBootApplication.java
@@ -0,0 +1,13 @@
+package ing.yisus.taller1springboot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Taller1SpringBootApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(Taller1SpringBootApplication.class, args);
+    }
+
+}
---
+++ src/test/java/ing/yisus/taller1springboot/Taller1SpringBootApplicationTests.java
@@ -0,0 +1,13 @@
+package ing.yisus.taller1springboot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class Taller1SpringBootApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
